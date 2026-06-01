5a2d03c70f511a18e7963674649b658af0ddf798
Neeraj Ojha <nojha1855@gmail.com>
2026-06-02 03:05:32 +0530

    del: creation of superuser from build.sh

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ChatRoom, ChatParticipant, Message
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'chat_room', 'content', 'created_at')
+    search_fields = ('sender__username', 'content')
+    list_filter = ('created_at',)
+
+admin.site.register(ChatRoom)
+admin.site.register(ChatParticipant)
+admin.site.register(Message, MessageAdmin)
